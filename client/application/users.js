67c9138c6024c57099ae92d81a1c50ce275b7067
stephenhammond <sphammond@gmail.com>
2015-03-24 15:04:20 -0700

    added config.js to handle github login. working on integration of github login api

--- client/application/users.js
+++ client/application/users.js
@@ -1,3 +1,16 @@
+
+
+
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
+
+
+// Template.users.helpers({
+//   users: [
+//     {name: "User 1"},
+//     {name: "User 2"},
+//   ]
+// });
+
+
--- server/users.js
+++ server/users.js
@@ -1,3 +1,32 @@
-Meteor.publish("user-info", function(id) {
-  return Meteor.users.find({_id: id}, {fields: {username: 1}});
+// Meteor.publish("userData", function () {
+//   return Meteor.users.find({_id: this.userId},
+//                            {fields: {'services': 1}});
+// });
+
+Accounts.onCreateUser(function (options, user) {
+    var accessToken = user.services.github.accessToken,
+        result,
+        profile;
+    result = Meteor.http.get("https://api.github.com/user", {
+        params: {
+            access_token: accessToken
+        }
+    });
+    if (result.error)
+        throw result.error;
+
+    profile = _.pick(result.data,
+        "login",
+        "name",
+        "avatar_url",
+        "url",
+        "company",
+        "blog",
+        "location",
+        "email",
+        "bio",
+        "html_url");
+
+    user.profile = profile;
+    return user;
 });
