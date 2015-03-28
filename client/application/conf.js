60f4ff3f6e1c72b6f3d19a4e1b695c176638024a
Eric Nguyen <hello@ericdesigns.ca>
2015-03-27 21:16:13 -0700

    Hard coded channel name so all participants end up on the same channel

--- client/application/conf.js
+++ client/application/conf.js
@@ -1,6 +1,25 @@
 Template.conf.onRendered( function(){
-	var connection = new RTCMultiConnection().connect();
-	document.querySelector('#openNewSessionButton').onclick = function() {
-  connection.open();
+
+	var connection = new RTCMultiConnection("commonchannel");
+	
+
+	// easiest way to customize what you need!
+	connection.session = {
+	    audio: true,
+	    video: true
 	};
+
+	// on getting local or remote media stream
+	connection.onstream = function(e) {
+	    document.body.appendChild(e.mediaElement);
+	};
+
+	// setup signaling channel
+	connection.connect("commonchannel");
+
+	// open new session
+	document.querySelector('#openNewSessionButton').onclick = function() {
+	    connection.open("commonchannel");
+	};
+
 })
