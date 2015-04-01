8e3fede120e808fd0e2ef69ece3e8bd57663b709
Mike Gazdag <mike@metalab.co>
2015-03-31 22:07:01 -0700

    Slight refactor on a few files

--- client/home.js
+++ client/home.js
@@ -1,15 +1,7 @@
-Template.home.events({
-  'click .button-app-start': function(e){
-    e.preventDefault();
-
-    var hash = Meteor.uuid();
-    var lobbyId = hash.substring(hash.length - 7);
-    Router.go('/app/' + lobbyId);
-  }
-});
-
-
 Template.home.onRendered(function() {
+  var hash = Meteor.uuid();
+  var lobbyId = hash.substring(hash.length - 7);
+  $('.button-app-start').attr('href', '/app/'+lobbyId);
 
  var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
   $(".masthead").css("height", viewportHeight + "px");
