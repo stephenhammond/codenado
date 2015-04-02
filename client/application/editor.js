4b0cd0358ae0689ae9092a36b22c3671e4748f41
David Vickery <david.vickery.23@gmail.com>
2015-04-02 00:56:51 -0700

    refactor menu to exclue it from draw tab

--- client/application/editor.js
+++ client/application/editor.js
@@ -5,15 +5,7 @@ Template.editor.onCreated(function(){
 Template.editor.onRendered( function() {
   Session.set('isViewingBoard', false);
   Session.set('activeTab', 0);
-  Deps.autorun(function() {
-    var docId = Session.get('lobbyId') +'-'+ Session.get('activeTab');
-    var editor = EditorConfig.findOne({docId: docId});
-    var syntaxSelected = 'javascript'
-    if (editor) {
-      syntaxSelected = editor.active_syntax
-    }
-    $('.editor-syntax').val(syntaxSelected);
-  });
+
   setTimeout(clickTab, 1500);
 });
 
