4b45143e2c229a465537fec37844557ed9b3f7ca
Mike Gazdag <mike@metalab.co>
2015-04-02 11:46:31 -0700

    Refactor broadcast code, and style video conrols

--- client/application/broadcast.js
+++ client/application/broadcast.js
@@ -2,13 +2,64 @@ Template.broadcast.events({
 
   'click .broadcast-list .dropdown-top-item': function (e) {
     e.preventDefault();
-    console.log('s');
     $(e.target).parents('.dropdown-select-list').addClass('dropdown-is-open');
   },
 
-  'blur .broadcast-list .dropdown-select-list': function(){
-    $('.dropdown-select-list.dropdown-is-open').removeClass('dropdown-is-open');
+  'blur .broadcast-list.dropdown-select-list': function(){
+    $('.broadcast-list.dropdown-select-list.dropdown-is-open').removeClass('dropdown-is-open');
+  },
+
+  'mousedown .broadcast-list .dropdown-menu a': function(e){
+    e.preventDefault();
+    $this = $(e.target);
+    var role = $this.data('role');
+
+    window.connection = new RTCMultiConnection();
+
+    connection.dontOverrideSession = true;
+    connection.session = {
+        audio: true,
+        video: true,
+        oneway: role == 'Anonymous Viewer'
+    };
+
+    connection.onstream = function(e) {
+        videos.appendChild(e.mediaElement);
+
+        if (e.type == 'remote') {
+            connection.askToShareParticipants();
+        }
+
+        if (connection.isInitiator && e.type == 'remote' && !e.session.oneway) {
+            connection.shareParticipants({
+                dontShareWith: e.userid
+            });
+        }
+    };
+
+    connection.onNewSession = function(session) {
+        if (role == 'Anonymous Viewer') {
+            session.join({
+                oneway: true
+            });
+        }
+
+        if (role == 'Co-Presenter') {
+            session.join();
+        }
+    };
+
+    if (role == 'Presenter') {
+      connection.open({
+          sessionid: connection.channel,
+          captureUserMediaOnDemand: false
+      });
+    } else {
+      connection.connect(connection.channel);
+    }
+
   }
+
 });
 
 Deps.autorun(function(){
@@ -25,69 +76,3 @@ Deps.autorun(function(){
     }
   });
 });
-
-Template.broadcast.onRendered( function(){
-
-	var selectRole = document.querySelector('#selectRole');
-	var launchConf = document.querySelector('#launchConf');
-	var videos = document.querySelector('#videos');
-
-	launchConf.onclick = function() {
-	  this.disabled = true;
-	  var role = selectRole.value;
-	  window.connection = new RTCMultiConnection();
-
-	  // dont-override-session allows you force RTCMultiConnection
-	  // to not override default session of participants;
-	  // by default, session is always overridden and set to the session coming from initiator!
-	  connection.dontOverrideSession = true;
-
-	  connection.session = {
-	      audio: true,
-	      video: true,
-	      oneway: role == 'Anonymous Viewer'
-	  };
-
-	  connection.onstream = function(e) {
-	      videos.appendChild(e.mediaElement);
-
-	      if (e.type == 'remote') {
-	          // because "viewer" joined room as "oneway:true"
-	          // initiator will NEVER share participants
-	          // to manually ask for participants;
-	          // call "askToShareParticipants" method.
-	          connection.askToShareParticipants();
-	      }
-
-	      // if you're moderator
-	      // if stream-type is 'remote'
-	      // if target user is broadcaster!
-	      if (connection.isInitiator && e.type == 'remote' && !e.session.oneway) {
-	          // call "shareParticipants" to manually share participants with all connected users!
-	          connection.shareParticipants({
-	              dontShareWith: e.userid
-	          });
-	      }
-	  };
-
-	  connection.onNewSession = function(session) {
-	      if (role == 'Anonymous Viewer') {
-	          session.join({
-	              oneway: true
-	          });
-	      }
-
-	      if (role == 'Co-Presenter') {
-	          session.join();
-	      }
-	  };
-
-	  if (role == 'Presenter')
-	      connection.open({
-	          sessionid: connection.channel,
-	          captureUserMediaOnDemand: false
-	      });
-	  else
-	      connection.connect(connection.channel);
-	};
-})
