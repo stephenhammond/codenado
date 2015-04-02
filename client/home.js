ff4a3cb2f2cd3a0756a0ea1114c5d94783d43f37
Mike Gazdag <mike@metalab.co>
2015-04-02 13:04:03 -0700

    Fix homepage responsiveness

--- client/home.js
+++ client/home.js
@@ -1,9 +1,12 @@
 Template.home.onRendered(function() {
-  var hash = Meteor.uuid();
-  var lobbyId = hash.substring(hash.length - 7);
-  $('.button-app-start').attr('href', '/app/'+lobbyId);
+    var hash = Meteor.uuid();
+    var lobbyId = hash.substring(hash.length - 7);
+    $('.button-app-start').attr('href', '/app/'+lobbyId);
 
- var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-  $(".masthead").css("height", viewportHeight + "px");
+  if (!navigator.userAgent.match(/iP(ad|hone|od)|android/gi)) {
+    var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    $(".masthead").css("height", viewportHeight + "px");
+  }
+
 
 });
